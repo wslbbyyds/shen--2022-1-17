493cbe38c1612b9ca96ca73a878fcc46a010eb1c
wslbbyyds <3044114668@qq.com>
2022-01-17 11:36:12 +0800

    头文件

---
+++ all.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <semaphore.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+#include <grp.h>
+#include <pwd.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/msg.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<poll.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include<sqlite3.h>
+
